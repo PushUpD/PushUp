911fba246e1b8bd005dfdbd24482d2deadc9744e
aymanab5 <aymn-an@hotmail.com>
2017-09-22 15:09:20 +0300

    Initial commit

---
+++ app/src/main/java/com/team1_abu_ghosh/pushup/MainActivity.java
@@ -0,0 +1,17 @@
+package com.team1_abu_ghosh.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void test(){
+        int i=1+1;
+    }
+}
